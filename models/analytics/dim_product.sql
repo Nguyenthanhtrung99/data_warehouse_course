61d8e3f2a0b7a4f82fcc95ab77f36cf7e554011c
Nguyenthanhtrung99 <88228219+Nguyenthanhtrung99@users.noreply.github.com>
2022-11-12 18:46:08 +0700

    Trungnt2 write model

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,33 @@
+WITH dim_product_source AS (
+
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+
+),
+
+dim_product_renamecolumn AS (
+
+  SELECT
+    stock_item_id     AS product_key,
+    stock_item_name   AS product_name,
+    brand             AS brand_name
+  FROM dim_product_source
+
+),
+
+dim_product_convertdatatype AS(
+
+  SELECT
+    CAST(stock_item_id AS INTEGER)    AS product_key,
+    CAST(stock_item_name AS STRING)   AS product_name,
+    CAST(brand AS STRING)             AS brand_name
+  FROM dim_product_renamecolumn
+)
+
 SELECT
-  CAST(stock_item_id AS INTEGER )    AS product_key,
-  CAST(stock_item_name AS STRING )   AS product_name,
-  CAST(brand AS STRING )             AS brand_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+  product_key,
+  product_name,
+  brand_name
+FROM dim_product_convertdatatype
+
+
